40538e205bd261176d8bd0b7c5bc8dc524c45fda
Samuel, Isaac (tuf97238) <isaac.samuel@temple.edu>
2019-12-21 14:50:08 -0500

    Init commit

---
+++ mlauto-tag.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: MLAuto Tag
+Plugin URI:  https://github.com/IsaacSamuel/MLAuto-Tag
+Description: This plugin uses machine learning (php-ml) to suggest auto-tags and auto-categorizations for posts. 
+Version:     0.1
+Author:      Isaac Samuel
+Author URI:  https://github.com/IsaacSamuel
+*/
