3700fb0b1359de0e823fd49a967ba064aa301c16
Isaac Samuel <getintouchwithisaac@gmail.com>
2019-12-21 21:29:44 -0500

    ArrayDataset loading correctly

--- mlauto-tag.php
+++ mlauto-tag.php
@@ -10,27 +10,63 @@ Author URI:  https://github.com/IsaacSamuel
 
 declare(strict_types=1);
 
-
 require 'class_loader.php';
+
 
 use mlauto\Post_Info_Aggregator;
 
+use Phpml\FeatureExtraction\TokenCountVectorizer;
+use Phpml\Tokenization\WhitespaceTokenizer;
+
+use Phpml\Dataset\ArrayDataset;
+use Phpml\CrossValidation\StratifiedRandomSplit;
 
 
 class MLAuto_Tag {
 
-	public function __construct() {
-		add_action('init', array(__CLASS__, 'main'));
+	private $dataset;
+
+
+	public function __toString() : string {
+		//toString function--for quick & dirty debugging
+		$o = "";
+
+		$o .= "Samples: <br>";
+		$o .= print_r($this->dataset->getSamples(), true);
+		$o .= "<br><br>";
+
+		$o .= "Targets: <br>";
+		$o .= print_r($this->dataset->getTargets(), true);
+		$o .= "<br><br>";
+
+		return $o;
 	}
 
-	function main() : void {
 
+
+	public function vectorize($info) :void {
+
+		//for each in outputs
+		$this->dataset = new ArrayDataset($info->features, array_column($info->outputs, 0));
+
+		echo $this->__toString();
+
+		
+		wp_die();
+
+	}
+
+	public function __construct() {
 		$taxonomies = array("category", "post_tag");
 
 		$info = new Post_Info_Aggregator($taxonomies);
+
+		$this->vectorize($info);
 	}
 
 }
+
+//add_action('init', array(__CLASS__, 'main'));
 
 new MLAuto_Tag();
 
