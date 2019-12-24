bff313340a7e2126abfef9c79e04b28412c85de7
Isaac Samuel <getintouchwithisaac@gmail.com>
2019-12-23 23:31:20 -0500

    Classifier serializing to file and saving meta info to db

--- mlauto-tag.php
+++ mlauto-tag.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 require 'class_loader.php';
 
 use mlauto\Model\PostInfoAggregator;
+use mlauto\Model\Classification;
+
 
 use mlauto\Analysis\Vectorizer;
 use mlauto\Analysis\Classifier;
@@ -28,11 +30,43 @@ use Phpml\Metric\ClassificationReport;
 
 
 
+define( 'MLAUTO_PLUGIN_URL', plugin_dir_path( __FILE__ ) );
 
 class MLAuto_Tag {
 
-	public function __construct() {
-		$taxonomies = array("category");//, "post_tag");
+	private function buildConfig() {
+
+		add_option( "MLAuto_version", "0.1" );
+
+		add_option('MLAuto_taxonomies', array("category"));
+		add_option('MLAuto_features', array("post_title"));
+		add_option('MLAuto_cost', 1.0);
+		add_option('MLAuto_gamma', null);
+		add_option('MLAuto_tolerance', .001);
+		add_option('MLAuto_cache_size', 100);
+		add_option('MLAuto_label_minimum_count', 1);
+		add_option('MLAuto_delete_old_classifiers', false);
+
+	}
+
+	private function getConfig() {
+		return array (
+			"taxonomies" => array("category"),
+			"cost" => floatval(get_option('MLAuto_cost')),
+			"gamma" => floatval(get_option('MLAuto_gamma')),
+			"tolerance" => floatval(get_option('MLAuto_tolerance')),
+			"cache_size" => intval(get_option('MLAuto_cache_size')),
+			"delete_old_classifiers" => get_option('MLAuto_delete_old_classifiers', false),
+			"specified_features" => get_option('MLAuto_features'),
+			"label_minimum_count" => get_option('MLAuto_label_minimum_count')
+		);
+	}
+
+	function runClassifier() {
+
+		$args = $this->getConfig();
+
+		$taxonomies = $args["taxonomies"];//, "post_tag");
 
 		$info = new PostInfoAggregator($taxonomies);
 
@@ -54,20 +88,38 @@ class MLAuto_Tag {
 				$test_samples = array_slice($vectorizer->vectorized_samples, 60);
 				$test_labels = array_slice($vectorized_labels, 60);
 
-				$classifier = new Classifier($train_samples, $train_labels);
-
+				$classifier = new Classifier($train_samples, $train_labels, $args);
+				$classifier->trainClassifier($train_samples, $train_labels, $args);
 
 				$predictedLabels = $classifier->predict($test_samples, $test_labels);
 
 				echo 'Target: ' . $target . " " . Accuracy::score($test_labels, $predictedLabels, true) . "<br>";
 
+				$args["taxonomy_name"] = $taxonomies[$i];
+				$args["accuracy"] = Accuracy::score($test_labels, $predictedLabels, true);
+				$args["tag_name"] = $target;
+				$args["training_percentage"] = .75;
+
+				//Classification::saveClassification($classifier, $args);
+
+				//var_dump($predictedLabels);
+
 
 			}
-
 			wp_die();
 
 		}
+	}
 
+	function init() {
+		$this->buildConfig();
+		Classification::intializeTable();
+	}
+
+	public function __construct() {
+		$this->init();
+
+		$this->runClassifier();
 
 	}
 
@@ -76,5 +128,53 @@ class MLAuto_Tag {
 //add_action('init', array(__CLASS__, 'main'));
 
 new MLAuto_Tag();
+
+/*
+function wporg_settings_init()
+{
+    // register a new setting for "reading" page
+    register_setting('reading', 'wporg_setting_name');
+ 
+    // register a new section in the "reading" page
+    add_settings_section(
+        'wporg_settings_section',
+        'WPOrg Settings Section',
+        'wporg_settings_section_cb',
+        'reading'
+    );
+ 
+    // register a new field in the "wporg_settings_section" section, inside the "reading" page
+    add_settings_field(
+        'wporg_settings_field',
+        'WPOrg Setting',
+        'wporg_settings_field_cb',
+        'reading',
+        'wporg_settings_section'
+    );
+}
+ 
+/**
+ * register wporg_settings_init to the admin_init action hook
+ 
+add_action('admin_init', 'wporg_settings_init');
+
+
+// section content cb
+function wporg_settings_section_cb()
+{
+    echo '<p>WPOrg Section Introduction.</p>';
+}
+ 
+// field content cb
+function wporg_settings_field_cb()
+{
+    // get the value of the setting we've registered with register_setting()
+    $setting = get_option('wporg_setting_name');
+    // output the field
+    ?>
+    <input type="text" name="wporg_setting_name" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+    <?php
+}
+*/
 
 ?>
--- src/Model/Classification.php
+++ src/Model/Classification.php
@@ -4,61 +4,79 @@ declare(strict_types=1);
 
 namespace mlauto\Model;
 
-	//Classification table
-	//name of taxonomy
-	//name of tag
-	//location of classifier
-	//date
-	//version
-	//specified features
-	//runtime
 
 class Classification {
 
 	public static function intializeTable() {
+
 		global $wpdb;
 		$charset_collate = $wpdb->get_charset_collate();
 		$table_name = $wpdb->prefix . 'MLAutoTag_Classifications';
 
-		$sql = "CREATE TABLE $table_name (
-			  id mediumint(9) NOT NULL AUTO_INCREMENT,
-			  created_at datetime NOT NULL,
-			  custom_name tinytext NOT NULL,
-			  taxonomy_name tinytext NOT NULL,
-			  tag_name tinytext NOT NULL,
-			  location_of_serialized_object tinytext NOT NULL,
-			  accuracy float,
-			  specified_features TEXT,
-			  PRIMARY KEY  (public_key)
-			) $charset_collate;";
+		//If table does not exist
+		if(!$wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name) {	
 
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		dbDelta( $sql );
+			$sql = "CREATE TABLE $table_name (
+				  id mediumint(9) NOT NULL AUTO_INCREMENT,
+				  created_at datetime NOT NULL,
+				  custom_name tinytext NOT NULL,
+				  taxonomy_name tinytext NOT NULL,
+				  tag_name tinytext NOT NULL,
+				  location_of_serialized_object tinytext NOT NULL,
+				  accuracy float,
+				  gamma float,
+				  cost float,
+				  tolerance float,
+				  training_percentage float,
+				  specified_features TEXT,
+				  PRIMARY KEY  (id)
+				) $charset_collate;";
+
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
+		}
+
 	}
 
-	public function saveClassification(array $args) {
+	public function saveClassification(object &$classifier, array $args) {
 		global $wpdb;
 
-		//$tag_name = $args["tag_name"];
-		//$taxonomy_name = $args["taxonomy_name"];
-		//$specified_features = impolode($args["specified_features"], ", ");
-		//accuracy = $args["accuracy"];
+		$tag_name = $args["tag_name"];
+		$taxonomy_name = $args["taxonomy_name"];
+		$specified_features = maybe_serialize($args["specified_features"]);
+		$accuracy = $args["accuracy"];
+		$gamma = $args["gamma"];
+		$tolerance = $args["tolerance"];
+		$cost = $args["cost"];
+		$training_percentage = $args["training_percentage"];
 
-		//$custom_name = ($args["custom_name"] ? $args["custom_name"] : timestamp);
+		$custom_name = ($args["custom_name"] ? $args["custom_name"] : current_time( 'timestamp' ));
 
-		//$location_of_serialized_object = custom_name / taxonomy_name / tag_name
+		$dir_of_serialized_object = 'bin/' . $custom_name . '/'.  $taxonomy_name . '/';
 
-		
+		$create_dir = mkdir( MLAUTO_PLUGIN_URL . $dir_of_serialized_object, 0777, true);
+
+
+		$location_of_serialized_object = $dir_of_serialized_object . trim($tag_name);
+
+
+		$classifier->saveToFile(MLAUTO_PLUGIN_URL . $location_of_serialized_object);
+
+
 		$table_name = $wpdb->prefix . 'MLAutoTag_Classifications';
 		
 		$wpdb->insert( 
 			$table_name, 
 			array( 
-				'created_at' => current_time( 'mysql' ), 
+				'created_at' => current_time( 'timestamp' ), 
 				'custom_name' => $custom_name,
 				'taxonomy_name' => $taxonomy_name,
 				'accuracy' => $accuracy,
+				'gamma' => $gamma,
+				'tolerance' => $tolerance,
 				'tag_name' => $tag_name,
+				'cost' => $cost,
+				'training_percentage' => $training_percentage,
 				'location_of_serialized_object' => $location_of_serialized_object,
 				'specified_features' => $specified_features,
 			) 
@@ -79,7 +97,7 @@ class Classification {
 		$wpdb->delete( 
 			$table_name, 
 			array( 
-				'id' => $id;
+				'id' => $id
 			) 
 		);
 	}
